08442063b8233c80a474325d6095a197100ce81f
Akari <arvin.lum@hotmail.com>
2025-02-22 00:18:23 +0800

    Latest Version

--- js/index.js
+++ js/index.js
@@ -1,15 +1,19 @@
-const title = document.querySelector('.title')
-const text = 'I Have Something'.split('')
+const title = document.querySelector('.title');
+
+// Text content
+const text = '我刚才确实做错了，没有考虑到你的感受，让你难过了，对不起。我不应该在没有提前告诉你的情况下和异性打游戏，我理解你的感受，以后一定会注意，不让你有这种不舒服的感觉。'.split('');
+
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
-    title.innerHTML += `<span>${text[index]}<span/>`
+    title.innerHTML += `<span>${text[index]}</span>`; // Fixed closing tag
   } else {
-    title.innerHTML += `<span style='margin-right: 20px;'><span/>`
+    title.innerHTML += `<span style='margin-right: 20px;'>&nbsp;</span>`; // Add a non-breaking space
   }
 }
 
+// Apply animation delay
 const textElements = document.querySelectorAll('.title span');
 textElements.forEach((element) => {
-  const randomDelay = Math.random() * 3; // Menghasilkan delay acak antara 0 hingga 3 detik
+  const randomDelay = Math.random() * 3; // Generate random delay between 0 to 3 seconds
   element.style.animationDelay = `${randomDelay}s`;
 });
--- js/main.js
+++ js/main.js
@@ -3,15 +3,19 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Only For Clara').split('')
+    const titles = ('送给我最爱的女人 林嘉宜 我爱你').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
     function appendTitle() {
       if (index < titles.length) {
+        // Insert a line break before "我爱你"
+        if (titles[index] === '我' && titles.slice(index, index + 3).join('') === '我爱你') {
+          titleElement.innerHTML += '<br>';
+        }
         titleElement.innerHTML += titles[index];
         index++;
-        setTimeout(appendTitle, 300); // 1000ms delay
+        setTimeout(appendTitle, 300); // 300ms delay
       }
     }
 
